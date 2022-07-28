5e30e1292a6fc7f6c6664e062ca8c0d4ee922868
Pahud Hsieh <pahudnet@gmail.com>
2022-07-28 21:32:15 +0000

    init

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
-import { App, Stack, StackProps } from 'aws-cdk-lib';
+import {
+  App, Stack, StackProps,
+  aws_eks as eks,
+  aws_ec2 as ec2,
+} from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 
 export class MyStack extends Stack {
   constructor(scope: Construct, id: string, props: StackProps = {}) {
     super(scope, id, props);
+
+    const vpc = ec2.Vpc.fromLookup(this, 'Vpc', { isDefault: true });
+
+    new eks.FargateCluster(this, 'Cluster', {
+      vpc,
+      version: eks.KubernetesVersion.V1_21,
+    });
 
     // define resources here...
   }
